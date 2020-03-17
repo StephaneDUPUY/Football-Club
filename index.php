277341bbc60c7d057c592d3e034fbaeaf7aba11d
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-03-17 14:09:14 +0100

    html and php part to give category relating age

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>UFC</title>
+    </head>
+    <body>
+
+        <h1>United Football Club</h1>
+        <h2>Section enfants (de 6 à 14 ans)</h2>
+        
+        <?php
+            // check age exist
+            if (isset($_GET['age'])):
+            // retrieve age and store it
+                $age = $_GET['age'];
+                // check age and give category
+                if ($age < 6):
+                    $category = "Trop jeune";
+                elseif ($age < 8):
+                    $category = "Poussin";
+                elseif ($age < 10):
+                    $category = "Pupille";
+                elseif ($age < 12):
+                    $category = "Minime";
+                elseif ($age < 15):
+                    $category = "Cadet";
+                else:
+                    $category = "Trop âgé";
+                endif;
+        ?>
+        <!-- display age -->
+        <p>Âge de l'enfant : <?=$age?></p>
+        <!-- display category -->
+        <h3>Catégorie : <?=$category?></h3>
+        <?php
+            endif;
+        ?>
+
+        <form action="index.php" method="get">
+            <label for="age">Quel est l'âge de votre enfant ?</label>
+            <input type="number" name="age" id="age" value="<?=@$age?>"> <!-- @ for no error message -->
+            <button type="submit">Calculer</button>
+        </form>
+        <p>Âge de l'enfant : <?=$age?></p>
+        <h3>Catégorie : <?=$category?></h3>
+
+    </body>
+</html>
